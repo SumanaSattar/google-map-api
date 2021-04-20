fb8396fcc218f3435261a82eaf3cfe2df7a0fa86
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-20 11:40:44 +0200

    google map api added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import {
+  InfoWindow,
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+import Geocode from 'react-geocode';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+Geocode.setApiKey("AIzaSyDjm0gEoKzdVhBbGQHQ7OE3KIdV_zDShpc")
+
+class App extends Component {
+  state = {
+    address:"",
+    city:"",
+    area:"",
+    state:"",
+    zoom:15,
+    height:400,
+    mapPosition:{
+      lat:0,
+      lng:0,
+    },
+    markerPosition:{
+      lat:0,
+      lng:0,
+    }
+  }
+
+  onMarkerDrag = (event) => {
+    let newLat = event.latLng.lat();
+    let newLng = event.latLng.lng();
+    console.log(newLat)
+    console.log(newLng)
+    Geocode.fromLatLng(newLat,newLng)
+    .then(response=>{
+      console.log(response)
+    })
+  }
+  render () {
+    const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+      <GoogleMap
+        defaultZoom={14}
+        defaultCenter={{ lat: 59.334591, lng: 18.063240 }}
+      >
+        <Marker draggable={true} onDragEnd={this.onMarkerDrag}
+          position={{ lat: 59.325695, lng: 18.071869 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <InfoWindow><div>hello</div></InfoWindow>
+        </Marker>
+      </GoogleMap>
+    ));
+    return (
+      <MapWithAMarker
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjm0gEoKzdVhBbGQHQ7OE3KIdV_zDShpc&v=3.exp&libraries=geometry,drawing,places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+ 
 }
 
 export default App;
